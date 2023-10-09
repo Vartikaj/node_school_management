eb6522ccfc576b109b09cb787e4a1343126c0122
vartikaj <vartikajohari57@gmail.com>
2023-10-09 17:44:00 +0530

    Changes related to the model data

--- controllers/admin-route.js
+++ controllers/admin-route.js
@@ -5,21 +5,27 @@ const asyncHandler = (fn) => (req, res, next) => {
 const registrationForm = require('../models/registrationForm');
 
 exports.postRegistrationData = asyncHandler(async (req, res, next) => {
-
     try {
         const regData = new registrationForm(req.body);
-        // console.log(req);
-        // regData.firstName = "Vrinda";
         const insertUserData = await regData.save();
-
-        console.log(insertUserData)
         res.status(200).json({
             success: true,
             data : insertUserData,
         })
     } catch (error) {
-        console.error('Error in postData:', error);
-        next(error);
+        res.status(200).json({
+            success: false,
+            data : error.message,
+        })
     }
 });
+
+exports.postLoginForm = asyncHandler(async (req, res, next) => {
+    try{
+        const logData = new registrationForm(req.body);
+        
+    } catch (error) {
+
+    } 
+})
 
--- models/registrationForm.js
+++ models/registrationForm.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validatorPackage = require('validator')
+const uniqueValidator = require('mongoose-unique-validator')
 const Schema = mongoose.Schema;
 
 const lstTransport = new Schema({
@@ -33,26 +34,39 @@ const lstguardianDetailSchema = new Schema({
     relation: String,
     salutation: String,
     name: String,
-    gender: String,
-    age: Number,
-    contact_no: {
+    gender: {
+        type: String,
+        enum: {
+            values : ['Female', 'Male', 'Others'],
+            message : '{VALUE} is not a valid gender name',
+        },
+        required: true,
+    },
+    age: {
         type: Number,
         trim: true,
+        min: [18, 'age must be grater then 18'],
+        max: [60, 'age must be less then 60']
+    },
+    contact_no: {
+        type: String,
+            minLength: [10, "no should have minimum 10 digits"],
+            maxLength: [10, "no should have maximum 10 digits"],
+            match: [/\d{10}/, "no should only have digits"],
+        trim: true,
         required: true,
-        unique: true,
-        min: [10, 'Number must be 10 digit long']
+        index: true,
+        unique: true
     },
     email: {
         type: String,
         trim: true,
         required: true,
         unique: true,
+        caseInsenstive : true,
         validate: {
-            validator: validatorPackage.email,
-            
+            validator: validatorPackage.isEmail,
         }
-
-
     },
     blood_group: String,
     school_employee: Boolean,
@@ -60,6 +74,9 @@ const lstguardianDetailSchema = new Schema({
     IsActive: Boolean,
     created_at: Date
 });
+
+
+
 
 const lstContactDetailSchema = new Schema({
     contactype: String,
@@ -79,9 +96,32 @@ const lstContactDetailSchema = new Schema({
 
 const registrationFormSchema = new Schema({
     salutation: String,
-    firstName: String,
-    middleName: String,
-    lastName: String,
+    firstName: {
+        type: String,
+        maxLength: [30, 'First name length not more then 30 character long'],
+        trim: true
+    },
+    middleName: {
+        type: String,
+        maxLength: [30, 'Middle name length not more then 30 charcters long']
+    },
+    lastName: {
+        type: String,
+        maxLength: [30, 'Last name length not more then 30 characters long'],
+        trim: true
+    },
+    username: {
+        type: String,
+        maxLength: [30, 'Username length not more then 30 characters long'],
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        maxLength: [30, 'Password length not more then 30 characters long'],
+        unique: true,
+        trim: true,
+    },
     genderCode: Number,
     genderDisplay: String,
     dob: Date,
@@ -94,8 +134,23 @@ const registrationFormSchema = new Schema({
     library_facility : Boolean,
     active : Boolean
 })
+registrationFormSchema.plugin(uniqueValidator, { message : '{PATH}: {VALUE} is already exists!!' });
 
 const registrationForm = mongoose.model('registration', registrationFormSchema);
+
+
+
+// registrationForm.init().
+//   then(() => registrationForm.create(dup)).
+//   catch(error => {
+//     // `U2.create()` will error, but will *not* be a mongoose validation error, it will be
+//     // a duplicate key error.
+//     // See: https://masteringjs.io/tutorials/mongoose/e11000-duplicate-key
+//     console.log('error');
+//   });
+
+
+
 
 //IF WE WANT TO CREATE MULTIPLE COLLECTION THEN WE USE THIS
 // const lstguardianDetailModel = mongoose.model('lstguardianDetail', lstguardianDetailSchema);
